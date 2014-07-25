e1dbcdd521701a4ca19b6cae3f9e58a1ba4d43a1
Benoit Delbosc <bdelbosc@nuxeo.com>
2014-07-25 23:29:14 +0200

    NXP-14811: fixing shutdown

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,6 +43,7 @@ public class Main {
         for (Runnable checker: checkers) {
             pool.execute(checker);
         }
+        pool.shutdown();
     }
 
     private static void registerListener() {
